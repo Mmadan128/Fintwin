2c99904cd2c2666f16a1f41fe53ae6a095bca386
Mmadan128 <mohitmadan128@gmail.com>
2025-02-10 22:30:30 +0530

    removed debuggin

--- simulations/forms.py
+++ simulations/forms.py
@@ -42,3 +42,6 @@ class SavingPlanForm(forms.Form):
     monthly_savings = forms.DecimalField(label="Monthly Savings (INR)", max_digits=10, decimal_places=2)
     expected_rate_of_return = forms.DecimalField(label="Expected Rate of Return (%)", max_digits=5, decimal_places=2)
     years_to_save = forms.IntegerField(label="Years to Save", min_value=1)
+
+class StockForm(forms.Form):
+    stock_symbols = forms.CharField(label='Stock Symbols (comma separated)', max_length=200)
