61b02f5e4b9bca15d04a62c9eb41e15f8f0f6eca
Mmadan128 <mohitmadan128@gmail.com>
2025-02-10 18:51:31 +0530

    settings iss

--- financialweb/wsgi.py
+++ financialweb/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for financialweb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Point to settings.py inside 'financialweb' app
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financialweb.settings')
 
 application = get_wsgi_application()
