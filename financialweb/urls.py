1d213f29a52b6c5aa5df0eee83973d216149e909
Mmadan128 <mohitmadan128@gmail.com>
2025-02-11 18:55:26 +0530

    fingpt response

--- financialweb/urls.py
+++ financialweb/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('newswidget/', include('newswidget.urls')), 
     path('simulations/', include('simulations.urls')), # Include the URLs from your app
+    path('mainpage/', views.mainpage, name='mainpage'),
 ]
